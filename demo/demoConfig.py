6cb54b170d588db2058f9a0646480ab202156eb3
Matthew Dennis <matthew.dennis@rogers.com>
2021-11-19 11:40:20 -0500

    Fixing up the demos

---
+++ demo/demoConfig.py
@@ -0,0 +1,7 @@
+# some thing we might want to change 
+#serverPort    = 7193 # this is what I am running in developement
+#serverUrl     = f"https://localhost:{serverPort}/v1/"
+#serverPort    = 5193 # this is what I am running in developement
+serverPort    = 5000 # this is what I am running in docker
+serverUrl     = f"http://localhost:{serverPort}/v1/"
+verifySslCert = False # my development cert is self-signed so don't verify
